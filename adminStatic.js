63c52ec5c42d7a89c2138af6bea24c044ebdd78c
rynvts <aryanvats00@gmail.com>
2023-08-24 07:05:47 +0530

    main

---
+++ adminStatic.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $.ajax({
+        url: "https://localhost:7143/existingClientsData",
+        type: 'GET',
+        // added data type
+        // data: JSON.stringify(newClientData),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            //    "Access-Control-Request-Method" : "*",
+            "Accept": "*",
+            "Content-Type": "application/json",
+            "Authorization" : "Bearer " + sessionStorage.token
+        },
+
+        success: function printData(res) {
+            alert("Client list loaded Successfully");
+            // console.log(res);
+            res = JSON.parse(res);
+            console.log(res);
+            for(var i=0; i<res.length; i++){
+                document.getElementById(res[i].id).value = res[i].Questions;
+            }
+        },
+        error: function (er) {
+            alert("errrorr");
+            alert(JSON.stringify(er));
+        }
+    });
+});
